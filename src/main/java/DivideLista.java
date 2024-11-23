359e3f64911a250d86e89d2c1ec8e7d6552da459
Artcraft <arturoao@uma.es>
2024-11-23 16:15:49 +0100

    Fixed DivideLista, all tests passed

--- src/main/java/DivideLista.java
+++ src/main/java/DivideLista.java
@@ -1,34 +1,35 @@
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class DivideLista {
-	
+
 	/**
-	 * 
+	 *
 	 * @param datos lista de entrada
 	 * @param k     longitud de una de las listas solución
 	 * @param a   lista solución 1 (salida)
 	 * @param b   lista solución 2 (salida)
 	 * @return  la diferencia entre las dos listas
-	 * 
+	 *
 	 */
 	public static int resolverVoraz(int []datos, int k, List<Integer> a, List<Integer> b) {
-		List<Integer> lista = new ArrayList<>();
-		for(int num : datos){
-			lista.add(num);
+		int diffTotal = 0;
+		Arrays.sort(datos);
+
+		if(k>datos.length/2){
+			k=datos.length-k;
 		}
-
-		Collections.sort(lista, Collections.reverseOrder());
-
-		b.addAll(lista.subList(0, k));
-		a.addAll(lista.subList(k, lista.size()));
-
-		int sumaB = b.stream().mapToInt(Integer::intValue).sum();
-		int sumaA = a.stream().mapToInt(Integer::intValue).sum();
-
-		return sumaB - sumaA;
+		for(int i = 0; i<datos.length; i++){
+			if(i<k){
+				a.add(i);
+				diffTotal-=datos[i];
+			}else{
+				b.add(i);
+				diffTotal+=datos[i];
+			}
+		}
+		return diffTotal;
 	}
 }
